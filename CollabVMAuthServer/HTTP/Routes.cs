2fa1a122f1413d8b17bf7d9b214df4376908fb33
Elijah R <elijah@elijahr.dev>
2024-04-08 22:02:26 -0400

    that again because im fucking stupid

--- CollabVMAuthServer/HTTP/Routes.cs
+++ CollabVMAuthServer/HTTP/Routes.cs
@@ -1043,7 +1043,7 @@ public static class Routes
         {
             await Program.Database.RegisterAccount(payload.username, payload.email, dob, payload.password, true, ip, null);
             var token = Utilities.RandomString(32);
-            await Program.Database.CreateSession(user.Username, token, ip);
+            await Program.Database.CreateSession(payload.username, token, ip);
             return Results.Json(new RegisterResponse
             {
                 success = true,
